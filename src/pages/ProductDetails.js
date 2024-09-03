809ba6056c6a2d6967102a914a5f5f3c16774a77
Akhilraj <akhilakhilraj18@gmail.com>
2024-09-03 20:43:39 +0530

    update client

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -90,7 +90,7 @@ const ProductDetails = () => {
           {relatedProducts?.map((p) => (
             <div className="card m-2" style={{width: '18rem'}} key={p._id}>
               <img 
-                src={`/api/v1/product/product-photo/${p._id}`} 
+                src={`https://ecommerce-server-coral-two.vercel.app/api/v1/product/product-photo/${p._id}`} 
                 className="card-img-top" 
                 alt={p.name} 
               />
